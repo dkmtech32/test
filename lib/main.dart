a4e2926f3070b6cb5021a5c4bd1f2d414be99ed7
dkmtech32 <dkmtien123@gmail.com>
2024-03-15 15:44:52 +0700

    firebase connected

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/core/themes.dart';
 import 'package:flutter_app/views/screens/auth/login_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main(List<String> args) {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
